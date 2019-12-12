d33c77bd36a91accb5e8f9ec399d117a01fb566e
kksrapmly <trust_2336@yahoo.co.jp>
2019-12-12 05:54:59 +0000

    コントローラ追加

---
+++ app/controllers/public/public_users_controller.rb
@@ -0,0 +1,20 @@
+class Public::PublicUsersController < ApplicationController
+  
+
+  def confirm
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+end
---
+++ app/helpers/admin/admin_arrivals_helper.rb
@@ -0,0 +1,2 @@
+module Admin::AdminArrivalsHelper
+end
