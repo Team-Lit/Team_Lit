c01c3c39dfafb91b8d2537e4aa8933ee6900cf52
yuudaihasegawa <yuudaihasegawa0218@gmail.com>
2019-12-21 10:38:51 +0000

    order バリデーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,16 +8,6 @@ class Order < ApplicationRecord
 
   enum delivery_status: {"未配送":0, "配送済み":1}
 
-  # validates :product_id, presence: true
-  # validates :end_user_id, presence: true
-  # validates :tax_rate, presence: true
-  # validates :delivery_charge, presence: true
-  # validates :total_price, presence: true
-  # validates :payment_mothed, presence: true
-  validates :zip, presence: true
-  # , length: { is: 7 }
-  validates :address, presence: true
-  validates :address_name, presence: true
   acts_as_paranoid
 
   
